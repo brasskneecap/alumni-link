fe0c736a9b2f9aaeeedec7c05765023f89a640a0
Morgan <121578632+morganharris39@users.noreply.github.com>
2025-05-11 19:01:39 -0600

    added proper spacing between components

--- portal/src/components/LoginPage/SigninPage.vue
+++ portal/src/components/LoginPage/SigninPage.vue
@@ -2,22 +2,22 @@
   <div class="signin-container">
     <h3>Sign in</h3>
     <TextField 
-      class="input-field"
+      class="email-field"
       label="Email"
       placeholder="Enter your email address"
       icon="mdi-email-outline"
     />
     <TextField 
-      class="input-field"
+      class="password-field"
       label="Password"
       placeholder="Enter your password"
       icon="mdi-lock-outline"
     />
     <div class="signin-functions">
-      <v-checkbox label="Remember Me"></v-checkbox>
+      <v-checkbox label="Remember Me" density="compact" class="signin-checkbox"></v-checkbox>
       <a href="#">Forgot Password?</a>
     </div>
-    <v-btn rounded="xl" size="x-large" class="text-subtitle-1 font-weight-medium">Login</v-btn>
+    <v-btn rounded="xl" size="x-large" class=" login-btn text-subtitle-1 font-weight-medium">Login</v-btn>
   </div>
 </template>
 
@@ -29,17 +29,44 @@ import TextField from '../input-fields/TextField.vue';
 @use '@/variables.scss' as *;
 
 .signin-container {
+  width: 26.8rem;
+  margin: 8.25rem auto;
 
-  // margin: 8rem 13rem 14rem 8rem;
+  h3 {
+    padding-bottom: 8rem;
+    font-size: 1.875rem;
+  }
 
-  .input-field {
+  .email-field, .password-field {
     padding: 0;
+  }
+
+  .password-field {
+    margin-top: 3rem;
   }
 
   .signin-functions {
     display: flex;
     justify-content: space-between;
     align-items: center;
+
+    .signin-checkbox {
+      display: flex;
+      margin: 0;
+      padding: 0;
+    }
+
+    a {
+      padding: 0;
+      margin: 0;
+    }
+  }
+
+  .login-btn {
+    width: 26.8rem;
+    background-color: black;
+    color: white;
+    margin-top: 3.75rem;
   }
 }
 </style>
