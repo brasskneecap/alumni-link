ea2efc662b04cd27a62adc719e7ea2f1c0dc7247
Morgan <121578632+morganharris39@users.noreply.github.com>
2025-06-24 15:00:43 -0600

    al-12 started creating list using dynamic data and vuetify list component

---
+++ portal/src/components/Assignments/AssignmentsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-list lines="two">  
+    <v-list-header>WEEK 1</v-list-header>
+    <v-list-item
+      v-for="(item, i) in assignments"
+      :key="i"
+      :value="item"
+      :title="item.name"
+      :subtitle="item.description"
+      :date="item.dueDate"
+      color="primary"
+    >
+      <template v-slot:prepend>
+        <v-icon icon="mdi-file-document-multiple-outline"></v-icon>
+      </template>
+      <template v-slot:append>
+        <span>{{ item.dueDate }}</span>
+      </template>
+    </v-list-item>
+  </v-list>
+</template>
+
+<!-- :date="formatDate(item.dueDate)" for later -->
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+// import { formatDate } from '@/utils/formatters' //will work once the formatDate file is merged
+
+const store = useStore()
+
+const assignments = computed(() => store.getters["assignments/assignments"])
+
+</script>
