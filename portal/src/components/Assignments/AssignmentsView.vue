4c0057ac39b65341c7d2cd1042f93b54beaf2b4b
Morgan <121578632+morganharris39@users.noreply.github.com>
2025-07-12 16:53:39 -0600

    al-28 made clicking on list items functional for student view, also started filling assignment view with data

---
+++ portal/src/components/Assignments/AssignmentsView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>
+        <h1 class="title">{{ assignment.name }}</h1>
+        <p>Due {{ formatDateMDY(assignment.dueDate) }}</p>
+        <p>{{ assignment.description }}</p>
+    </div>
+    <div>
+      <h2>Submit Assignment</h2>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  assignment: Object
+})
+
+function formatDateMDY(dateStr) {
+  const date = new Date(dateStr)
+  const month = String(date.getMonth() + 1)
+  const day = String(date.getDate())
+  const year = date.getFullYear().toString().slice(-2)
+  return `${month}/${day}/${year}`
+}
+</script>
+
+<style lang="scss" scoped>
+@use '@/variables.scss' as *;
+
+.title {
+  font-size: 24px;
+  font-weight: 700;
+}
+
+</style>
