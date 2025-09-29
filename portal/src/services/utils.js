36baeeaaeb3520e11d221ca486efc3cfdf3d7fbf
brasskneecap <codyanderson91@gmail.com>
2025-09-28 18:22:24 -0600

    AL enabled switch for dev and prod

--- portal/src/services/utils.js
+++ portal/src/services/utils.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "https://alumni-link-api.onrender.com";
+// const API_BASE_URL = "http://localhost:8080";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const request = async ({ url, body, options }) => {
   try {
